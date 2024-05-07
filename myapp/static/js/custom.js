f0c95fb2dbdb1589d3e3ba2e4d8a3da883e1ce83
Ziad <ziadaldisi2@gmail.com>
2024-05-07 22:02:58 +0300

    Frontend task: Home page without login

---
+++ myapp/static/js/custom.js
@@ -0,0 +1,317 @@
+(function($) {
+
+	'use strict';
+	// Mean Menu
+	$('.mean-menu').meanmenu({
+		meanScreenWidth: "991"
+	});
+	
+	// Header Sticky, Go To Top JS
+	$(window).on('scroll', function() {
+		// Header Sticky JS
+		if ($(this).scrollTop() >150){  
+			$('.navbar-area').addClass("is-sticky");
+		}
+		else{
+			$('.navbar-area').removeClass("is-sticky");
+		};
+
+		// Go To Top JS
+		var scrolled = $(window).scrollTop();
+		if (scrolled > 300) $('.go-top').addClass('active');
+		if (scrolled < 300) $('.go-top').removeClass('active');
+	});
+
+	// Hero Slider
+	$('.hero-slider').owlCarousel({
+		loop: true,
+		margin: 0,
+		nav: false,
+		mouseDrag: false,
+		thumbs: true,
+		thumbsPrerendered: true,
+		items: 1,
+		dots: false,
+		autoHeight: true,
+		autoplay: true,
+		smartSpeed: 1500,
+		autoplayHoverPause: true,
+		navText: [
+			"<i class='ri-arrow-left-s-line'></i>",
+			"<i class='ri-arrow-right-s-line'></i>",
+		],
+		responsive:{
+			0: {
+				nav: true,
+			},
+			767: {
+				nav: true,
+			},
+			768: {
+				nav: true,
+			},
+			992: {
+				nav: false,
+			},
+		},
+	});
+
+	// Counselor Slider JS
+	$('.counselor-slider').owlCarousel({
+		items: 1,
+		loop: true,
+		margin: 0,
+		nav: true,
+		dots: false,
+		autoplay: true,
+		smartSpeed: 1000,
+		autoplayHoverPause: true,
+		navText: [
+			"<i class='ri-arrow-left-s-line'></i>",
+			"<i class='ri-arrow-right-s-line'></i>",
+		],
+	});
+	
+	// Project Slider JS
+	$('.project-slider').owlCarousel({
+		loop: true,
+		margin: 30,
+		nav: true,
+		dots: false,
+		autoplay: true,
+		smartSpeed: 1000,
+		autoplayHoverPause: true,
+		navText: [
+			"<i class='ri-arrow-left-s-line'></i>",
+			"<i class='ri-arrow-right-s-line'></i>",
+		],
+		responsive: {
+			0: {
+				items: 1,
+			},
+			576: {
+				items: 1,
+			},
+			768: {
+				items: 2,
+			},
+			992: {
+				items: 3,
+			},
+			1200: {
+				items: 4,
+			},
+			1700: {
+				items: 5,
+			},
+		},
+	});
+
+	// Project Slider JS
+	$('.our-another-slider').owlCarousel({
+		items: 1,
+		loop: true,
+		margin: 30,
+		nav: true,
+		dots: false,
+		autoplay: true,
+		smartSpeed: 1000,
+		autoplayHoverPause: true,
+		navText: [
+			"<i class='ri-arrow-left-s-line'></i>",
+			"<i class='ri-arrow-right-s-line'></i>",
+		],
+	});
+	
+	// Click Event JS
+	$('.go-top').on('click', function() {
+		$("html, body").animate({ scrollTop: "0" }, 50);
+	});
+
+	// FAQ Accordion JS
+	$('.accordion').find('.accordion-title').on('click', function(){
+		// Adds Active Class
+		$(this).toggleClass('active');
+		// Expand or Collapse This Panel
+		$(this).next().slideToggle('fast');
+		// Hide The Other Panels
+		$('.accordion-content').not($(this).next()).slideUp('fast');
+		// Removes Active Class From Other Titles
+		$('.accordion-title').not($(this)).removeClass('active');		
+	});
+
+	// Count Time JS
+	function makeTimer() {
+		var endTime = new Date("november  30, 2026 17:00:00 PDT");			
+		var endTime = (Date.parse(endTime)) / 1000;
+		var now = new Date();
+		var now = (Date.parse(now) / 1000);
+		var timeLeft = endTime - now;
+		var days = Math.floor(timeLeft / 86400); 
+		var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+		var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+		var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+		if (hours < "10") { hours = "0" + hours; }
+		if (minutes < "10") { minutes = "0" + minutes; }
+		if (seconds < "10") { seconds = "0" + seconds; }
+		$("#days").html(days + "<span>Days</span>");
+		$("#hours").html(hours + "<span>Hours</span>");
+		$("#minutes").html(minutes + "<span>Minutes</span>");
+		$("#seconds").html(seconds + "<span>Seconds</span>");
+	}
+	setInterval(function() { makeTimer(); }, 300);
+
+	// Preloader
+	$(window).on('load', function() {
+		$('.preloader').addClass('preloader-deactivate');
+	}) 
+
+	// Subscribe form JS
+	$(".newsletter-form").validator().on("submit", function (event) {
+		if (event.isDefaultPrevented()) {
+		// handle the invalid form...
+			formErrorSub();
+			submitMSGSub(false, "Please enter your email correctly.");
+		} else {
+			// everything looks good!
+			event.preventDefault();
+		}
+	});
+	function callbackFunction (resp) {
+		if (resp.result === "success") {
+			formSuccessSub();
+		}
+		else {
+			formErrorSub();
+		}
+	}
+	function formSuccessSub(){
+		$(".newsletter-form")[0].reset();
+		submitMSGSub(true, "Thank you for subscribing!");
+		setTimeout(function() {
+			$("#validator-newsletter").addClass('hide');
+		}, 4000)
+	}
+	function formErrorSub(){
+		$(".newsletter-form").addClass("animated shake");
+		setTimeout(function() {
+			$(".newsletter-form").removeClass("animated shake");
+		}, 1000)
+	}
+	function submitMSGSub(valid, msg){
+		if(valid){
+			var msgClasses = "validation-success";
+		} else {
+			var msgClasses = "validation-danger";
+		}
+		$("#validator-newsletter, #validator-newsletter-2").removeClass().addClass(msgClasses).text(msg);
+	}
+	
+	// AJAX MailChimp JS
+	$(".newsletter-form").ajaxChimp({
+		url: "https://Envy Theme.us20.list-manage.com/subscribe/post?u=60e1ffe2e8a68ce1204cd39a5&amp;id=42d6d188d9", // Your url MailChimp
+		callback: callbackFunction
+	});
+
+	// Others Option For Responsive JS
+	$(".others-option-for-responsive .dot-menu").on("click", function(){
+		$(".others-option-for-responsive .container .container").toggleClass("active");
+	});
+
+	// ScrollCue JS
+    scrollCue.init();
+
+	// Popup Video JS
+	$('.popup-youtube, .popup-vimeo').magnificPopup({
+		disableOn: 300,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false,
+	});
+
+	// Gallery Popup JS
+	$('.gallery-popup').each(function() {
+		$(this).magnificPopup({
+			delegate: 'a', 
+			type: 'image',
+			gallery: {
+			  enabled:true
+			}
+		});
+	});
+
+	// Odometer JS
+	$('.odometer').appear(function(e) {
+		var odo = $(".odometer");
+		odo.each(function() {
+			var countNumber = $(this).attr("data-count");
+			$(this).html(countNumber);
+		});
+	});
+
+	// Search Popup JS
+	$('.close-btn').on('click', function() {
+		$('.search-overlay').fadeOut();
+		$('.search-btn').show();
+		$('.close-btn').removeClass('active');
+	});
+	$('.search-btn').on('click', function() {
+		$(this).hide();
+		$('.search-overlay').fadeIn();
+		$('.close-btn').addClass('active');
+	});
+
+	$(document).ready(function(){
+		$('[data-toggle="tooltip"]').tooltip();   
+	});
+
+	// MixItUp JS
+	try {
+        var mixer = mixitup('#Container', {
+            controls: {
+                toggleDefault: 'none'
+            }
+        });
+    } catch (err) {}
+
+})(jQuery);
+// -------------------------------------------------
+// Our code 
+// Favorite List page | For story rating 
+document.addEventListener("DOMContentLoaded", function() {
+    var storyRate = 4; // Assuming storyRate is a variable representing the story rating
+    var ratingContainer =  document.querySelectorAll('.rating');
+	
+	// we should use the rate parameter {Story rate from DB} to be here --> if (i <= storyRate) (instead of storyRate)
+	ratingContainer.forEach(function(container, rate) {
+		for (var i = 1; i <= 5; i++) {
+			var starIcon = document.createElement('i');
+			starIcon.classList.add('ri');
+			if (i <= storyRate) {
+				starIcon.classList.add('ri-star-fill');
+			} else {
+				starIcon.classList.add('ri-star-line');
+			}
+			container.appendChild(starIcon);
+		}
+	});
+});
+
+// Favorite List page | Heart icon (remove from fav)
+document.addEventListener("DOMContentLoaded", function() {
+    var heartIcons = document.querySelectorAll('.heart_Icon');
+
+    heartIcons.forEach(function(heartIcon) {
+        heartIcon.addEventListener('click', function() {
+            if (heartIcon.classList.contains('ri-heart-fill')) {
+                heartIcon.classList.remove('ri-heart-fill');
+                heartIcon.classList.add('ri-heart-line');
+            } else {
+                heartIcon.classList.remove('ri-heart-line');
+                heartIcon.classList.add('ri-heart-fill');
+            }
+        });
+    });
+});
