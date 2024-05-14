fe50fc92c73cf5ecd26e28f6b665a77cd3d32c8a
Rand <r.farhoud2000@gmail.com>
2024-05-14 15:14:48 +0300

    Back-End task: Login and logout

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('register',views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),  
-    path('logout',views.logout_view),
+    path('login', views.login_view, name='login'),
+    path('logout',views.logout_view, name='logout'),
     path('not_login',views.not_login),
     path('profile/', views.profile_view, name='profile'),
     path('update_password', views.update_password),
