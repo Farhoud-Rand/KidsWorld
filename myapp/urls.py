592d4d468f9a3a756f964e63cad1a2755577546e
Rand <r.farhoud2000@gmail.com>
2024-05-14 15:23:09 +0300

    Back-End task: profile page

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     path('register',views.register_view, name='register'),
     path('login', views.login_view, name='login'),
     path('logout',views.logout_view, name='logout'),
-    path('not_login',views.not_login),
-    path('profile/', views.profile_view, name='profile'),
-    path('update_password', views.update_password),
+    path('not_login',views.not_login, name='not_login'),
+    path('profile', views.profile_view, name='profile'),
+    path('update_password', views.update_password, name='update_password'),
     path('contact',views.contact_view, name='contact'),
 
 ]
